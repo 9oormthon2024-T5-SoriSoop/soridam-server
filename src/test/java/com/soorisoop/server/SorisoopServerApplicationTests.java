033534b355069d6cd89b9f54b286e5d1cb4e636d
minjo-on <alswns11346@gmail.com>
2024-12-10 23:33:47 +0900

    Initial commit

---
+++ src/test/java/com/soorisoop/server/SorisoopServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soorisoop.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SorisoopServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
