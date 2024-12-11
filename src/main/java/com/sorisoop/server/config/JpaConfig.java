dc3c9b3d3fae2934c201950a572b955a18b59fd7
minjo-on <alswns11346@gmail.com>
2024-12-11 18:15:40 +0900

    feat: point 객체 생성 설정

--- src/main/java/com/sorisoop/server/config/JpaConfig.java
+++ src/main/java/com/sorisoop/server/config/JpaConfig.java
@@ -1,5 +1,6 @@
 package com.sorisoop.server.config;
 
+import org.locationtech.jts.geom.GeometryFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,5 +19,10 @@ public class JpaConfig {
 	@Bean
 	public JPAQueryFactory jpaQueryFactory() {
 		return new JPAQueryFactory(entityManager);
+	}
+
+	@Bean
+	public GeometryFactory geometryFactory() {
+		return new GeometryFactory();
 	}
 }
