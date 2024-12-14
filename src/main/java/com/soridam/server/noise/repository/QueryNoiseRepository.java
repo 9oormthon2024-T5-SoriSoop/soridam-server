285d5f3edf5f70748b9bd501a087c91f3e76e39c
minjo-on <alswns11346@gmail.com>
2024-12-14 15:37:12 +0900

    Update QueryNoiseRepository.java

--- src/main/java/com/soridam/server/noise/repository/QueryNoiseRepository.java
+++ src/main/java/com/soridam/server/noise/repository/QueryNoiseRepository.java
@@ -1,8 +1,17 @@
 package com.soridam.server.noise.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.locationtech.jts.geom.Point;
 import org.springframework.stereotype.Repository;
 
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.soridam.server.noise.domain.Noise;
+import com.soridam.server.noise.domain.QNoise;
+import com.soridam.server.noise.dto.enums.NoiseLevel;
+import com.soridam.server.noise.dto.enums.Radius;
 
 import lombok.RequiredArgsConstructor;
 
@@ -10,4 +19,22 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class QueryNoiseRepository {
 	private final JPAQueryFactory jpaQueryFactory;
+
+	public List<Noise> findByAvgDecibleAndPoint(Point point, Radius radius, NoiseLevel noiseLevel) {
+		QNoise noise = QNoise.noise;
+
+		return jpaQueryFactory.selectFrom(noise)
+			.where(
+				noise.avgDecibel.between(noiseLevel.getMinDecibel(), noiseLevel.getMaxDecibel()),
+				Expressions.booleanTemplate(
+					"ST_DWithin({0}, ST_SetSRID(ST_MakePoint({1}, {2}), 4326) 5181, {3})",
+					noise.point,
+					point.getX(),
+					point.getY(),
+					radius.getRadiusInMeters()
+				),
+				noise.createdAt.after(LocalDateTime.now().minusHours(1))
+			)
+			.fetch(); // 결과를 리스트로 반환
+	}
 }
