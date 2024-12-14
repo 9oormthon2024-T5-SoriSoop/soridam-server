e11134cc73e09bd4569df455beedf561bb808bc2
minjo-on <alswns11346@gmail.com>
2024-12-14 22:50:15 +0900

    refactor: 사용하지 않는 메소드 삭제

--- src/main/java/com/soridam/server/noise/repository/JpaNoiseRepository.java
+++ src/main/java/com/soridam/server/noise/repository/JpaNoiseRepository.java
@@ -1,11 +1,8 @@
 package com.soridam.server.noise.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.soridam.server.noise.domain.Noise;
 
 public interface JpaNoiseRepository extends JpaRepository<Noise, Long> {
-	List<Noise> findByUserId(Long userId);
 }
