e59482d10b2ecf3920a17614fc078a24ea1f8117
minjo-on <alswns11346@gmail.com>
2024-12-14 19:15:55 +0900

    feat: noise 상세 조회 구현

--- src/main/java/com/soridam/server/user/controller/UserApiController.java
+++ src/main/java/com/soridam/server/user/controller/UserApiController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.soridam.server.noise.dto.response.NoiseSummaryListResponse;
+import com.soridam.server.noise.dto.response.NoiseSummaryResponse;
+import com.soridam.server.noise.service.NoiseService;
 import com.soridam.server.user.service.UserService;
 
 import lombok.RequiredArgsConstructor;
@@ -16,12 +18,21 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class UserApiController {
 	private final UserService userService;
+	private final NoiseService noiseService;
 
-	@GetMapping("/{userId}")
+	@GetMapping("/{id}")
 	public ResponseEntity<NoiseSummaryListResponse> getUserNoises(
-		@PathVariable Long userId
+		@PathVariable Long id
 	) {
-		NoiseSummaryListResponse response = userService.getUserNoises(userId);
+		NoiseSummaryListResponse response = userService.getUserNoises(id);
+		return ResponseEntity.ok(response);
+	}
+
+	@GetMapping("/noise/{id}")
+	public ResponseEntity<NoiseSummaryResponse> getUserDetailNoise(
+		@PathVariable Long id
+	) {
+		NoiseSummaryResponse response = noiseService.getNoise(id);
 		return ResponseEntity.ok(response);
 	}
 }
