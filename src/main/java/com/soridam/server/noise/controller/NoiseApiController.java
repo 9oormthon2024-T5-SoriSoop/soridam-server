dabf39533c1c058037a853e74a904ba75e23cf03
minjo-on <alswns11346@gmail.com>
2024-12-14 20:59:19 +0900

    Update NoiseApiController.java

--- src/main/java/com/soridam/server/noise/controller/NoiseApiController.java
+++ src/main/java/com/soridam/server/noise/controller/NoiseApiController.java
@@ -1,22 +1,22 @@
 package com.soridam.server.noise.controller;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.soridam.server.noise.dto.response.NoiseDetailResponse;
 import com.soridam.server.noise.dto.enums.NoiseLevel;
 import com.soridam.server.noise.dto.enums.Radius;
 import com.soridam.server.noise.dto.request.NoiseCreateRequest;
 import com.soridam.server.noise.dto.request.NoiseSearchListRequest;
 import com.soridam.server.noise.dto.response.NoiseCreateResponse;
+import com.soridam.server.noise.dto.response.NoiseDetailResponse;
 import com.soridam.server.noise.dto.response.NoiseListResponse;
 import com.soridam.server.noise.service.NoiseService;
 
@@ -34,6 +34,7 @@ public class NoiseApiController {
 		@RequestParam double y
 	){
 		NoiseDetailResponse response = noiseService.getDetailNoise(x, y);
+		return ResponseEntity.ok(response);
   }
 
   @PostMapping
