16c2492c19e3cdde18d6c88552475334cc980a76
minjo-on <alswns11346@gmail.com>
2024-12-14 16:37:25 +0900

    Update NoiseService.java

--- src/main/java/com/soridam/server/noise/service/NoiseService.java
+++ src/main/java/com/soridam/server/noise/service/NoiseService.java
@@ -1,7 +1,13 @@
 package com.soridam.server.noise.service;
 
-import org.springframework.stereotype.Service;
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.soridam.server.noise.domain.Noise;
+import com.soridam.server.noise.dto.response.NoiseSummaryListResponse;
+import com.soridam.server.noise.dto.response.NoiseSummaryResponse;
 import com.soridam.server.noise.repository.JpaNoiseRepository;
 import com.soridam.server.noise.repository.QueryNoiseRepository;
 
@@ -12,4 +18,14 @@ import lombok.RequiredArgsConstructor;
 public class NoiseService {
 	private final JpaNoiseRepository jpaNoiseRepository;
 	private final QueryNoiseRepository queryNoiseRepository;
+
+	@Transactional(readOnly = true)
+	public NoiseSummaryListResponse getUserNoises(Long userId) {
+		List<Noise> results = jpaNoiseRepository.findByUserId(userId);
+		List<NoiseSummaryResponse> responses = results.stream()
+			.map(NoiseSummaryResponse::from)
+			.toList();
+
+		return NoiseSummaryListResponse.of(responses);
+	}
 }
