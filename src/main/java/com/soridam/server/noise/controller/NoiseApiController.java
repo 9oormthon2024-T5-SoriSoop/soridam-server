e2c16be8f4d2122a2c2e87f51144b779a6529c3a
minjo-on <alswns11346@gmail.com>
2024-12-14 16:15:11 +0900

    Update NoiseApiController.java

--- src/main/java/com/soridam/server/noise/controller/NoiseApiController.java
+++ src/main/java/com/soridam/server/noise/controller/NoiseApiController.java
@@ -1,7 +1,15 @@
 package com.soridam.server.noise.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.soridam.server.noise.dto.response.NoiseDetailResponse;
+import com.soridam.server.noise.service.NoiseService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -9,4 +17,14 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RequestMapping("/api/noise")
 public class NoiseApiController {
+	private final NoiseService noiseService;
+
+	@GetMapping
+	public ResponseEntity<NoiseDetailResponse> getDetailNoise(
+		@PathVariable double x,
+		@PathVariable double y
+	){
+		NoiseDetailResponse response = noiseService.getDetailNoise(x, y);
+		return ResponseEntity.ok(response);
+	}
 }
