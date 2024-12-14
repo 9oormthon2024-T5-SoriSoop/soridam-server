1848a7b5fc68421b99a17558d4b773f5f7468c0f
Minjoon Park <alswns11346@gmail.com>
2024-12-14 19:20:24 +0900

    feature/#17 noise 상세 조회 구현 (#26)

--- src/main/java/com/soridam/server/noise/service/NoiseService.java
+++ src/main/java/com/soridam/server/noise/service/NoiseService.java
@@ -1,13 +1,11 @@
 package com.soridam.server.noise.service;
 
-import java.util.List;
-
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.soridam.server.noise.domain.Noise;
-import com.soridam.server.noise.dto.response.NoiseSummaryListResponse;
 import com.soridam.server.noise.dto.response.NoiseSummaryResponse;
+import com.soridam.server.noise.exception.NoiseNotFoundException;
 import com.soridam.server.noise.repository.JpaNoiseRepository;
 import com.soridam.server.noise.repository.QueryNoiseRepository;
 
@@ -20,12 +18,10 @@ public class NoiseService {
 	private final QueryNoiseRepository queryNoiseRepository;
 
 	@Transactional(readOnly = true)
-	public NoiseSummaryListResponse getUserNoises(Long userId) {
-		List<Noise> results = jpaNoiseRepository.findByUserId(userId);
-		List<NoiseSummaryResponse> responses = results.stream()
-			.map(NoiseSummaryResponse::from)
-			.toList();
+	public NoiseSummaryResponse getNoise(Long id) {
+		Noise noise = jpaNoiseRepository.findById(id)
+			.orElseThrow(NoiseNotFoundException::new);
 
-		return NoiseSummaryListResponse.of(responses);
+		return NoiseSummaryResponse.from(noise);
 	}
 }
