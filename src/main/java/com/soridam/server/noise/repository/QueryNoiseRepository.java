eb69a77b60b182f3092661380da46f9b100d0112
minjo-on <alswns11346@gmail.com>
2024-12-14 16:15:08 +0900

    Update QueryNoiseRepository.java

--- src/main/java/com/soridam/server/noise/repository/QueryNoiseRepository.java
+++ src/main/java/com/soridam/server/noise/repository/QueryNoiseRepository.java
@@ -1,8 +1,14 @@
 package com.soridam.server.noise.repository;
 
+import java.util.List;
+
+import org.locationtech.jts.geom.Point;
 import org.springframework.stereotype.Repository;
 
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.soridam.server.noise.domain.Noise;
+import com.soridam.server.noise.domain.QNoise;
 
 import lombok.RequiredArgsConstructor;
 
@@ -10,4 +16,20 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class QueryNoiseRepository {
 	private final JPAQueryFactory jpaQueryFactory;
+
+	public List<Noise> getNearbyNoises(Point point) {
+		QNoise noise = QNoise.noise;
+
+		return jpaQueryFactory.selectFrom(noise)
+			.where(
+				Expressions.booleanTemplate(
+					"ST_DWithin({0}, ST_SetSRID(ST_MakePoint({1}, {2}), 5181), {3})",
+					noise.point,
+					point.getX(),
+					point.getY(),
+					50
+				)
+			)
+			.fetch();
+	}
 }
