b58db600f2cf635b797ca8034a9f1555cff8c792
minjo-on <alswns11346@gmail.com>
2024-12-20 23:13:56 +0900

    feat: user login request dto 생성

--- soridam-api/src/main/java/sorisoop/soridam/api/auth/presentation/AuthApiController.java
+++ soridam-api/src/main/java/sorisoop/soridam/api/auth/presentation/AuthApiController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import sorisoop.soridam.api.auth.application.AuthService;
@@ -14,14 +17,19 @@ import sorisoop.soridam.api.auth.presentation.response.jwt.JwtResponse;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Auth", description = "로그인 API")
 @RequestMapping("/api/auth")
-public class AuthController {
+public class AuthApiController {
 	private final AuthService authService;
 
+	@Operation(summary = "JWT 로그인 API", description = """
+			- Description : 이 API는 로그인 시 JWT를 발급합니다.
+		""")
+	@ApiResponse(responseCode = "200")
 	@PostMapping("/jwt/login")
 	public ResponseEntity<JwtResponse> login(
-		@RequestBody
 		@Valid
+		@RequestBody
 		JwtLoginRequest request
 	) {
 		JwtResponse response = authService.jwtLogin(request);
--- soridam-api/src/main/java/sorisoop/soridam/api/auth/presentation/request/jwt/JwtLoginRequest.java
+++ soridam-api/src/main/java/sorisoop/soridam/api/auth/presentation/request/jwt/JwtLoginRequest.java
@@ -1,7 +1,17 @@
 package sorisoop.soridam.api.auth.presentation.request.jwt;
 
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+
 public record JwtLoginRequest(
-	String email,
+	@Schema(description = "이메일", example = "alswns11346@kgu.ac.kr", requiredMode = REQUIRED)
+	@NotBlank
+	String userId,
+
+	@Schema(description = "비밀번호", example = "password1234!", requiredMode = REQUIRED)
+	@NotBlank
 	String password
 ) {
 }
