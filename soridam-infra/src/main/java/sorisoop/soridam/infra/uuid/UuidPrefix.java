f42d276845330f6b07875698fa1fb3aae0a465f9
minjo-on <alswns11346@gmail.com>
2024-12-21 15:09:32 +0900

    refactor: prefix 수정

--- soridam-infra/src/main/java/sorisoop/soridam/infra/uuid/UuidPrefix.java
+++ soridam-infra/src/main/java/sorisoop/soridam/infra/uuid/UuidPrefix.java
@@ -6,11 +6,11 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum UuidPrefix {
-	USER("usr_"),
-	NOISE("nse_"),
-	SOUND("snd_"),
+	USER("user_"),
+	NOISE("noise_"),
+	SOUND("sound_"),
 	IMAGE("img_"),
-	DEFAULT("daf_");
+	DEFAULT("def_");
 
 	private final String prefix;
 }
