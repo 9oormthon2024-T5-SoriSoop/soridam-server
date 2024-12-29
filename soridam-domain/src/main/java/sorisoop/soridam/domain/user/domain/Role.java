5d01b548e0879cd277fd50cb4e2d10cbb92cfdb1
minjo-on <alswns11346@gmail.com>
2024-12-30 01:07:52 +0900

    refactor: role 접두사 제거

--- soridam-domain/src/main/java/sorisoop/soridam/domain/user/domain/Role.java
+++ soridam-domain/src/main/java/sorisoop/soridam/domain/user/domain/Role.java
@@ -6,10 +6,10 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum Role {
-	ROLE_ADMIN("관리자"),
-	ROLE_PAID_USER("유료 사용자"),
-	ROLE_USER("일반 사용자"),
-	ROLE_NOT_REGISTERED("미등록 사용자");
+	ADMIN("관리자"),
+	PAID_USER("유료 사용자"),
+	USER("일반 사용자"),
+	NOT_REGISTERED("미등록 사용자");
 
 	private final String description;
 }
