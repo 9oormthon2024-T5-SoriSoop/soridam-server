89dddd8c04b4fd0b3a00c5e745eec6a562dfa41f
minjo-on <alswns11346@gmail.com>
2025-01-20 14:18:36 +0900

    feat: kafka producer, consumer config 설정

---
+++ soridam-infra/src/main/java/sorisoop/soridam/infra/config/kafka/KafkaConsumerConfig.java
@@ -0,0 +1,44 @@
+package sorisoop.soridam.infra.config.kafka;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+@EnableKafka
+@Configuration
+public class KafkaConsumerConfig {
+
+	private static final String BOOTSTRAP_SERVERS = "172.18.0.101:9092";
+
+	private Map<String, Object> commonConsumerProps() {
+		Map<String, Object> props = new HashMap<>();
+		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+		return props;
+	}
+
+	@Bean
+	public ConsumerFactory<String, Object> noiseConsumer() {
+		Map<String, Object> props = new HashMap<>(commonConsumerProps());
+		props.put(ConsumerConfig.GROUP_ID_CONFIG, "noise-events-group");
+		return new DefaultKafkaConsumerFactory<>(props);
+	}
+
+	@Bean(name = "noiseEventsGroup")
+	public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactoryGroup1() {
+		ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+		factory.setConsumerFactory(noiseConsumer());
+		return factory;
+	}
+}
+
---
+++ soridam-infra/src/main/java/sorisoop/soridam/infra/config/kafka/KafkaProducerConfig.java
@@ -0,0 +1,41 @@
+package sorisoop.soridam.infra.config.kafka;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+@EnableKafka
+@Configuration
+public class KafkaProducerConfig {
+	private static final String BOOTSTRAP_SERVERS = "172.18.0.101:9092";
+
+	@Bean
+	public ProducerFactory<String, Object> producerFactory() {
+		Map<String, Object> configProps = new HashMap<>();
+		configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+		configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+		return new DefaultKafkaProducerFactory<>(configProps);
+	}
+
+	@Bean
+	public KafkaTemplate<String, Object> kafkaTemplate() {
+		return new KafkaTemplate<>(producerFactory());
+	}
+
+	@Bean
+	public NewTopic createEventsTopic() {
+		return new NewTopic("noise-events-topic", 3, (short) 1);
+	}
+
+}
