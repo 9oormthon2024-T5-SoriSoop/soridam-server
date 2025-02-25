4267849d3c7edd7ba2f8440514a66388e500d3cc
minjo-on <alswns11346@gmail.com>
2025-02-25 19:41:02 +0900

    refactor: 토큰 발급 로직 수정

--- soridam-api/src/main/java/sorisoop/soridam/api/auth/application/AuthFacade.java
+++ soridam-api/src/main/java/sorisoop/soridam/api/auth/application/AuthFacade.java
@@ -1,5 +1,7 @@
 package sorisoop.soridam.api.auth.application;
 
+import static sorisoop.soridam.globalutil.uuid.UuidPrefix.USER;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -7,10 +9,14 @@ import lombok.RequiredArgsConstructor;
 import sorisoop.soridam.api.auth.presentation.request.jwt.JwtLoginRequest;
 import sorisoop.soridam.api.auth.presentation.request.jwt.RefreshTokenRequest;
 import sorisoop.soridam.api.auth.presentation.request.oauth.OidcLoginRequest;
-import sorisoop.soridam.auth.jwt.application.JwtService;
+import sorisoop.soridam.auth.jwt.application.JwtProvider;
 import sorisoop.soridam.auth.jwt.response.JwtResponse;
 import sorisoop.soridam.auth.oauth.google.GoogleOidcService;
 import sorisoop.soridam.auth.oauth.kakao.KakaoOidcService;
+import sorisoop.soridam.domain.refresh.application.RefreshTokenService;
+import sorisoop.soridam.domain.refresh.domain.RefreshToken;
+import sorisoop.soridam.domain.user.application.UserCommandService;
+import sorisoop.soridam.domain.user.application.UserQueryService;
 import sorisoop.soridam.domain.user.domain.User;
 
 @Service
@@ -18,27 +24,50 @@ import sorisoop.soridam.domain.user.domain.User;
 public class AuthFacade {
 	private final KakaoOidcService kakaoOidcService;
 	private final GoogleOidcService googleOidcService;
-	private final JwtService jwtService;
+	private final JwtProvider jwtProvider;
+	private final UserQueryService userQueryService;
+	private final UserCommandService userCommandService;
+	private final RefreshTokenService refreshTokenService;
 
+	@Transactional
 	public JwtResponse jwtLogin(JwtLoginRequest request) {
-		return jwtService.jwtLogin(request.email(), request.password());
+		User user = userQueryService.getByEmail(request.email());
+		userCommandService.login(user, request.password());
+		JwtResponse response = getToken(user);
+		refreshTokenService.save(user.extractUuid(), response.refreshToken());
+
+		return response;
 	}
 
 	@Transactional
 	public JwtResponse kakaoLogin(OidcLoginRequest idToken) {
 		User user = kakaoOidcService.processLogin(idToken.idToken());
 		user.updateLastLoginTime();
-		return jwtService.getToken(user);
+		return getToken(user);
 	}
 
 	@Transactional
 	public JwtResponse googleLogin(OidcLoginRequest idToken) {
 		User user = googleOidcService.processLogin(idToken.idToken());
 		user.updateLastLoginTime();
-		return jwtService.getToken(user);
+		return getToken(user);
 	}
 
+	@Transactional(readOnly = true)
 	public JwtResponse reissue(RefreshTokenRequest request) {
-		return jwtService.reissue(request.refreshToken());
+		RefreshToken refreshToken = refreshTokenService.getToken(request.refreshToken());
+		String userId = refreshToken.getUserId();
+
+		User user = userQueryService.getById(USER.getPrefix() + userId);
+		JwtResponse response = getToken(user);
+		refreshTokenService.save(userId, response.refreshToken());
+		return response;
+	}
+
+	private JwtResponse getToken(User user) {
+		String refreshToken = jwtProvider.generateRefreshToken(user.extractUuid(), user.getRole());
+		String accessToken = jwtProvider.generateAccessToken(user.extractUuid(), user.getRole());
+
+		return JwtResponse.of(accessToken, refreshToken);
 	}
 }
--- soridam-domain/src/main/java/sorisoop/soridam/domain/user/application/UserCommandService.java
+++ soridam-domain/src/main/java/sorisoop/soridam/domain/user/application/UserCommandService.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
 import sorisoop.soridam.domain.user.domain.User;
 import sorisoop.soridam.domain.user.domain.UserRepository;
-import sorisoop.soridam.domain.user.exception.UserNotFoundException;
 
 @Service
 @RequiredArgsConstructor
@@ -25,9 +24,7 @@ public class UserCommandService {
 		return userRepository.save(user);
 	}
 
-	public User login(String email, String password){
-		User user = userRepository.findByEmail(email)
-			.orElseThrow(UserNotFoundException::new);
+	public User login(User user, String password){
 		user.isPasswordMatching(password, bCryptPasswordEncoder);
 		user.updateLastLoginTime();
 
