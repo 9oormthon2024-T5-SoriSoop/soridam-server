0ee92a311875ad84eb7896ef1a06944a668ce5bd
minjo-on <alswns11346@gmail.com>
2025-02-25 13:43:20 +0900

    refactor: infra 디렉토리 정리

--- soridam-infra/src/main/java/sorisoop/soridam/infra/config/data/jpa/JpaConfig.java
+++ soridam-infra/src/main/java/sorisoop/soridam/infra/config/data/jpa/JpaConfig.java
@@ -1,9 +1,8 @@
-package sorisoop.soridam.infra.config.jpa;
+package sorisoop.soridam.infra.config.data.jpa;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
@@ -12,7 +11,7 @@ import sorisoop.soridam.infra.config.base.SoriDamConfig;
 
 @EntityScan(basePackages = "sorisoop.soridam.domain")
 @Configuration
-@EnableJpaRepositories(basePackages = "sorisoop.soridam")
+/*@EnableJpaRepositories(basePackages = "sorisoop.soridam")*/
 public class JpaConfig implements SoriDamConfig {
 	private final EntityManager entityManager;
 
