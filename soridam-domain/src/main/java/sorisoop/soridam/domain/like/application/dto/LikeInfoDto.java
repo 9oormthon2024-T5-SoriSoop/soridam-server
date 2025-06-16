05944e7e6795aa28b2d519b6f8056b25ab3c1f9a
minjo-on <alswns11346@gmail.com>
2025-06-16 20:56:33 +0900

    refactor: redis 설정 수정

--- soridam-domain/src/main/java/sorisoop/soridam/domain/like/application/dto/LikeInfoDto.java
+++ soridam-domain/src/main/java/sorisoop/soridam/domain/like/application/dto/LikeInfoDto.java
@@ -1,18 +1,11 @@
 package sorisoop.soridam.domain.like.application.dto;
 
-import lombok.Builder;
-
-@Builder
 public record LikeInfoDto(
 	long targetId,
 	long likeCount,
 	boolean liked
 ) {
 	public static LikeInfoDto of(long targetId, long likeCount, boolean liked) {
-		return LikeInfoDto.builder()
-			.targetId(targetId)
-			.likeCount(likeCount)
-			.liked(liked)
-			.build();
+		return new LikeInfoDto(targetId, likeCount, liked);
 	}
 }
--- soridam-infra/src/main/java/sorisoop/soridam/infra/config/data/redis/RedisConfig.java
+++ soridam-infra/src/main/java/sorisoop/soridam/infra/config/data/redis/RedisConfig.java
@@ -11,6 +11,7 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -42,8 +43,8 @@ public class RedisConfig {
 	}
 
 	@Bean
-	public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
-		RedisTemplate<String, String> template = new RedisTemplate<>();
+	public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+		RedisTemplate<String, Object> template = new RedisTemplate<>();
 		template.setConnectionFactory(connectionFactory);
 		template.setKeySerializer(new StringRedisSerializer());
 		template.setHashKeySerializer(new StringRedisSerializer());
@@ -53,5 +54,10 @@ public class RedisConfig {
 		template.afterPropertiesSet();
 
 		return template;
+	}
+
+	@Bean
+	public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory connectionFactory) {
+		return new StringRedisTemplate(connectionFactory);
 	}
 }
