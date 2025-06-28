ce92be88eb4dfd4afc8ceb30e77a45339b014879
minjo-on <alswns11346@gmail.com>
2025-06-28 18:21:22 +0900

    feature: 클래스 작성

---
+++ soridam-domain/src/main/java/sorisoop/soridam/domain/activitylog/ActivityLog.java
@@ -0,0 +1,57 @@
+package sorisoop.soridam.domain.activitylog;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.data.elasticsearch.core.geo.GeoPoint;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@RequiredArgsConstructor
+@Document(indexName = "activity_log-#{T(java.time.LocalDate).now().format(T(java.time.format.DateTimeFormatter).ofPattern('yyyy-MM'))}")
+public class ActivityLog {
+	@Id
+	private String id;
+
+	@Field(type = FieldType.Long)
+	private final Long userId;
+
+	@Field(type = FieldType.Long)
+	private final Long placeId;
+
+	private final GeoPoint latlon;
+
+	@Field(type = FieldType.Keyword)
+	private final ActivityType activityType;
+
+	@Field(type = FieldType.Double)
+	private final Double customScore;
+
+	@Field(type = FieldType.Date)
+	private final LocalDateTime createdAt;
+
+	public Double getScore() {
+		return customScore != null ? customScore : activityType.getScore();
+	}
+
+	public static ActivityLog create(Long userId, Long placeId, double lat, double lon,
+		ActivityType activityType, Double customScore) {
+		return ActivityLog.builder()
+			.userId(userId)
+			.placeId(placeId)
+			.latlon(new GeoPoint(lat, lon))
+			.activityType(activityType)
+			.customScore(customScore)
+			.createdAt(LocalDateTime.now())
+			.build();
+	}
+}
---
+++ soridam-domain/src/main/java/sorisoop/soridam/domain/activitylog/ActivityType.java
@@ -0,0 +1,17 @@
+package sorisoop.soridam.domain.activitylog;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ActivityType {
+	VIEW(1.0),
+	LIKE(3.0),
+	REVIEW(5.0),
+	NOISE_REGISTER(4.0),
+	VISIT(2.0),
+	FAVORITE(4.0);
+
+	private final Double score;
+}
