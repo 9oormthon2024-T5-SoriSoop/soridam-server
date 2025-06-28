d33940eae5e67989743477625fb7eff9c704f8af
minjo-on <alswns11346@gmail.com>
2025-06-28 20:29:47 +0900

    feat: 조회시 로그 저장

--- soridam-domain/src/main/java/sorisoop/soridam/domain/activitylog/application/ActivityLogService.java
+++ soridam-domain/src/main/java/sorisoop/soridam/domain/activitylog/application/ActivityLogService.java
@@ -1,17 +1,35 @@
 package sorisoop.soridam.domain.activitylog.application;
 
+import org.locationtech.jts.geom.Point;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 import sorisoop.soridam.domain.activitylog.domain.ActivityLog;
 import sorisoop.soridam.domain.activitylog.domain.ActivityLogRepository;
+import sorisoop.soridam.domain.activitylog.domain.enums.ActivityType;
+import sorisoop.soridam.domain.place.place.domain.Place;
+import sorisoop.soridam.domain.user.user.domain.User;
 
 @Service
 @RequiredArgsConstructor
 public class ActivityLogService {
 	private final ActivityLogRepository activityLogRepository;
 
-	public void saveActivityLog(ActivityLog activityLog) {
+	public void save(User user, Place place, ActivityType activityType) {
+		Point location = place.getLocation();
+
+		double lon = location.getX();
+		double lat = location.getY();
+
+		ActivityLog activityLog = ActivityLog.create(
+			user.getId(),
+			place.getId(),
+			lat,
+			lon,
+			activityType,
+			activityType.getScore()
+		);
+
 		activityLogRepository.save(activityLog);
 	}
 }
--- soridam-domain/src/main/java/sorisoop/soridam/domain/activitylog/domain/ActivityLog.java
+++ soridam-domain/src/main/java/sorisoop/soridam/domain/activitylog/domain/ActivityLog.java
@@ -12,6 +12,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import sorisoop.soridam.domain.activitylog.domain.enums.ActivityType;
 
 @Getter
 @Setter
