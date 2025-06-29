bdd8fcae6c4e6be391b90e4b462e2b0b265423bb
minjo-on <alswns11346@gmail.com>
2025-06-30 00:39:52 +0900

    Update ActivityLog.java

--- soridam-domain/src/main/java/sorisoop/soridam/domain/activitylog/domain/ActivityLog.java
+++ soridam-domain/src/main/java/sorisoop/soridam/domain/activitylog/domain/ActivityLog.java
@@ -10,11 +10,9 @@ import org.springframework.data.elasticsearch.core.geo.GeoPoint;
 
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 import sorisoop.soridam.domain.activitylog.domain.enums.ActivityType;
 
 @Getter
-@Setter
 @Builder
 @Document(indexName = "activity_log-#{T(java.time.LocalDate).now().format(T(java.time.format.DateTimeFormatter).ofPattern('yyyy-MM'))}")
 public class ActivityLog {
